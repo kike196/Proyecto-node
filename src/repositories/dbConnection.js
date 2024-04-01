3e3ce3f5990f50d03d74e304ae2a78e4a0cbd342
Kike <enrique.aranaga@hotmail.com>
2024-04-01 14:53:52 -0400

    corrección dbconnection, footer

--- src/repositories/dbConnection.js
+++ src/repositories/dbConnection.js
@@ -1,11 +1,15 @@
 import { createPool } from "mysql2/promise";
-import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER } from "../config.js";
+import { DB_HOST,  DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } from "../config.js";
 
 export const pool = createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
+  port: DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 pool.getConnection()
